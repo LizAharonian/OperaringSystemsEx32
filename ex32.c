946f1e11ff8fc84ef0d9bd25d504228571f1a619
LizAharonian <lizaharonian@gmail.com>
2018-05-15 17:47:38 +0300

    finish

--- ex32.c
+++ ex32.c
@@ -35,7 +35,7 @@
 
 //declarations
 void handleFailure();
-int readCMDFile(char* cmdFile,char directoryPath[INPUT_SIZE], char inputFilePath[INPUT_SIZE],char outputFilePath [INPUT_SIZE]);
+void readCMDFile(char* cmdFile,char directoryPath[INPUT_SIZE], char inputFilePath[INPUT_SIZE],char outputFilePath [INPUT_SIZE]);
 typedef struct students {
     char name[INPUT_SIZE];
     char cFilePath[INPUT_SIZE];
@@ -65,16 +65,29 @@ int main(int argc, char **argv) {
     char inputFilePath[INPUT_SIZE] = {};
     char outputFilePath[INPUT_SIZE] = {};
     readCMDFile(argv[1], directoryPath, inputFilePath, outputFilePath);
-    printf("cmd file text:\n%s\n%s\n%s\n", directoryPath, inputFilePath, outputFilePath);
-    //todo: allocate dynamic memory
+    //dynamic memory allocation for students arr
     students* myStudents=(students *)malloc(STUSENTS_NUM * sizeof(students));
     if (myStudents==NULL) {
         handleFailure();
     }
     int i =0;
-
+    //explore students's dirs
     exploreSubDirs(directoryPath,myStudents,&i);
+    //grade the students with appropiate reason
     gradeStudents(myStudents,i,inputFilePath,outputFilePath);
+    //write the results to csv file
+    writeResultsToCsv(myStudents,i);
+    //free of memory allocation
+    free(myStudents);
+    return 0;
+}
+
+/**
+ * writeResultsToCsv function.
+ * @param myStudents - students arr
+ * @param i - number of students
+ */
+void writeResultsToCsv(students * myStudents, int i){
     int resultsCsvFD;
     if((resultsCsvFD=open("results.csv",O_CREAT | O_TRUNC | O_WRONLY, 0644))==-1)
     {
@@ -82,29 +95,24 @@ int main(int argc, char **argv) {
     }
     int j;
     for (j=0;j<i;j++) {
-        char concat[CONCAT] ={};
-        strcpy(concat,myStudents[j].name);
-        strcat(concat,",");
-        strcat(concat,myStudents[j].grade);
-        strcat(concat,",");
-        strcat(concat,myStudents[j].reson);
-        strcat(concat,"\n");
-        if(write(resultsCsvFD,concat,CONCAT)<0)
-        {
-           handleFailure();
+        char concat[CONCAT] = {};
+        strcpy(concat, myStudents[j].name);
+        strcat(concat, ",");
+        strcat(concat, myStudents[j].grade);
+        strcat(concat, ",");
+        strcat(concat, myStudents[j].reson);
+        strcat(concat, "\n");
+        if (write(resultsCsvFD, concat, CONCAT) < 0) {
+            handleFailure();
         }
     }
     close(resultsCsvFD);
-    free(myStudents);
 
-    //printf("liz");
-    return 0;
 }
 
 /**
  * callExecv function.
  * @param args - array for execvp function
- * @param isBackground - tells if parent need to wait to son
  * @return
  */
 void callExecv(char **args) {
@@ -123,6 +131,13 @@ void callExecv(char **args) {
     }
 }
 
+/**
+ * gradeStudents function.
+ * @param myStudents - students arr
+ * @param myStudentsSize - num of students
+ * @param inputFilePath - input file
+ * @param outputFilePath - output file
+ */
 void gradeStudents(students* myStudents,int myStudentsSize, char inputFilePath[INPUT_SIZE], char outputFilePath[INPUT_SIZE]) {
     int i;
     for (i = 0; i < myStudentsSize; i++) {
@@ -151,15 +166,6 @@ void gradeStudents(students* myStudents,int myStudentsSize, char inputFilePath[I
                 //run the program
                 runProgram(inputFilePath,myStudents,i,outputFilePath);
             }
-
-
-            //todo: delete all created files
-           /* if (unlink("a.out") == FAIL) {
-                handleFailure();
-            }*/
-
-
-
         }
     }
 
@@ -173,7 +179,6 @@ int isAOutExist() {
     if ((dip = opendir(cwd)) == NULL) {
         handleFailure();
     }
-
     while ((dit = readdir(dip)) != NULL) {
 
         if (strcmp(dit->d_name, "a.out") == 0) {
@@ -184,6 +189,13 @@ int isAOutExist() {
     return isAoutExist;
 }
 
+/**
+ * run program function.
+ * @param inputFilePath - unput file
+ * @param myStudents - students arr
+ * @param i - num of students
+ * @param outputFilePath - output file
+ */
 void runProgram(char inputFilePath[INPUT_SIZE],students* myStudents, int i,char outputFilePath[INPUT_SIZE]) {
     //make args for a.out
     char *args[INPUT_SIZE];
@@ -193,106 +205,88 @@ void runProgram(char inputFilePath[INPUT_SIZE],students* myStudents, int i,char 
     int stat, retCode;
     pid_t pid;
     pid = fork();
-     if (pid == 0) {  // son
-         int programOutputFD;
-         if ((programOutputFD = open("programOutput.txt", O_CREAT | O_TRUNC | O_WRONLY, 0644)) < 0) {
-             handleFailure();
-         }
-         int programInputFD;
-         if ((programInputFD = open(inputFilePath, O_RDONLY)) < 0) {
-             handleFailure();
-         }
+    if (pid == 0) {  // son
+        int programOutputFD;
+        if ((programOutputFD = open("programOutput.txt", O_CREAT | O_TRUNC | O_WRONLY, 0644)) < 0) {
+            handleFailure();
+        }
+        int programInputFD;
+        if ((programInputFD = open(inputFilePath, O_RDONLY)) < 0) {
+            handleFailure();
+        }
+        retCode = dup2(programInputFD, 0);
+        if (retCode == FAIL) {
+            handleFailure();
+        }
+        retCode = dup2(programOutputFD, 1);
+        if (retCode == FAIL) {
+            handleFailure();
+        }
+        retCode = execvp(args[0], &args[0]);
+        if (retCode == FAIL) {
+            handleFailure();
+        }
+        close(programInputFD);
+        close(programOutputFD);
+    } else {   //father
+        int value;
+        sleep(5);
+        pid_t returnPid = waitpid(pid, &value, WNOHANG);
+        //if a.out still running
+        if (returnPid ==0) {
+            strcpy(myStudents[i].grade, "0");
+            strcpy(myStudents[i].reson,TIMEOUT);
+        }else { //start comparisson
+            //make args for comp.out
+            char *args[INPUT_SIZE];
+            char operation[INPUT_SIZE] = "./comp.out";
+            args[0] = operation;
+            args[1] = "programOutput.txt";
+            args[2] = outputFilePath;
+            args[3] = NULL;
+            pid_t pid;
+            pid = fork();
+            if (pid == 0) {  // son
 
-         retCode = dup2(programInputFD, 0);
-         if (retCode == FAIL) {
-             handleFailure();
-         }
-         retCode = dup2(programOutputFD, 1);
-         if (retCode == FAIL) {
-             handleFailure();
-         }
-         retCode = execvp(args[0], &args[0]);
-         if (retCode == FAIL) {
-             handleFailure();
-         }
-         close(programInputFD);
-         close(programOutputFD);
-        /* retCode = dup2(0, 0);
-         if (retCode == FAIL) {
-             handleFailure();
-         }
-         retCode = dup2(1, 1);
-         if (retCode == FAIL) {
-             handleFailure();
-         }*/
-
-     } else {   //father
-         int value;
-         sleep(5);
-         pid_t returnPid = waitpid(pid, &value, WNOHANG);
-         //if a.out still running
-         if (returnPid ==0) {
-             strcpy(myStudents[i].grade, "0");
-             strcpy(myStudents[i].reson,TIMEOUT);
-         }else { //start comparisson
-             //make args for comp.out
-             char *args[INPUT_SIZE];
-             char operation[INPUT_SIZE] = "./comp.out";
-             args[0] = operation;
-             args[1] = "programOutput.txt";
-             args[2] = outputFilePath;
-             args[3] = NULL;
-             pid_t pid;
-             pid = fork();
-             if (pid == 0) {  // son
-
-                 retCode = execvp(args[0], &args[0]);
-                 if (retCode == FAIL) {
-                     handleFailure();
-                 }
-             } else {//father
-                 waitpid(pid, &value, 0);
-                 if ( WIFEXITED(value) ) {
-                     const int es = WEXITSTATUS(value);
-                     printf("exit status was %d\n", es);
-
-
-                     switch (es) {
-                         case 1:
-                             strcpy(myStudents[i].grade, "60");
-                             strcpy(myStudents[i].reson, BAD_OUTPUT);
-                             break;
-                         case 2:
-                             strcpy(myStudents[i].grade, "80");
-                             strcpy(myStudents[i].reson, SIMILAR_OUTPUT);
-                             break;
-                         case 3:
-                             strcpy(myStudents[i].grade, "100");
-                             strcpy(myStudents[i].reson, GREAT_JOB);
-                             break;
-                         default:
-                             break;
-                     }
-                 }
-                 if (unlink("a.out") == FAIL||unlink("programOutput.txt")) {
-                     handleFailure();
-                 }
-
-
-             }
-
-         }
-         /*else if (value!=0) {//running failed
-             myStudents[i].grade=0;
-             strcpy(myStudents[i].reson,TIMEOUT);
-         }*/
-
-     }
-
-    //todo:make comparison between the output and the expected output
-    //todo: close fd's and dirs!!
-
+                retCode = execvp(args[0], &args[0]);
+                if (retCode == FAIL) {
+                    handleFailure();
+                }
+            } else {//father
+                waitpid(pid, &value, 0);
+                if ( WIFEXITED(value) ) {
+                    const int es = WEXITSTATUS(value);
+                    switch (es) {
+                        case 1:
+                            strcpy(myStudents[i].grade, "60");
+                            strcpy(myStudents[i].reson, BAD_OUTPUT);
+                            break;
+                        case 2:
+                            strcpy(myStudents[i].grade, "80");
+                            strcpy(myStudents[i].reson, SIMILAR_OUTPUT);
+                            break;
+                        case 3:
+                            strcpy(myStudents[i].grade, "100");
+                            strcpy(myStudents[i].reson, GREAT_JOB);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                if (unlink("a.out") == FAIL||unlink("programOutput.txt")) {
+                    handleFailure();
+                }
+            }
+        }
+    }
 }
+
+/**
+ * exploreSubDirs function.
+ * @param directoryPath - dir path
+ * @param myStudents - my students arr
+ * @param i - num of students
+ */
 void exploreSubDirs(char directoryPath[INPUT_SIZE],students* myStudents,int* i) {
     DIR* dip;
     struct dirent* dit;
@@ -303,9 +297,6 @@ void exploreSubDirs(char directoryPath[INPUT_SIZE],students* myStudents,int* i) 
 
     while ((dit=readdir(dip))!=NULL) {
         if (dit->d_type == DT_DIR && strcmp(dit->d_name, ".") != 0 && strcmp(dit->d_name, "..") != 0) {
-            //debug only
-            printf("%s\n", dit->d_name);
-
             //fill the students array
             strcpy(myStudents[*i].cFilePath, "\0");
             strcpy(myStudents[*i].name, "\0");
@@ -321,12 +312,11 @@ void exploreSubDirs(char directoryPath[INPUT_SIZE],students* myStudents,int* i) 
             }
             strcat(subDir, dit->d_name);
             char cPath[INPUT_SIZE]={};
-
+            //find the c file path
             findTheCFile(subDir, myStudents, *i, cPath);
-
             strcpy(myStudents[*i].cFilePath, cPath);
-            printf("here,remmember play with the order%s",cPath);
             (*i)++;
+            //realloc in case is needed
             if ((*i)>=sizeOfStudentsArr) {
                 sizeOfStudentsArr+=STUSENTS_NUM;
                 myStudents = (students *)realloc(myStudents,sizeof(struct students)*sizeOfStudentsArr);
@@ -334,43 +324,34 @@ void exploreSubDirs(char directoryPath[INPUT_SIZE],students* myStudents,int* i) 
                     handleFailure();
                 }
             }
-
-        } /*else if (dit->d_type == DT_REG) {
-            int len = strlen(dit->d_name);
-            printf("%s\n", dit->d_name);
-            if (len >= 2 && dit->d_name[len - 1] == 'c' && dit->d_name[len - 2] == '.') {
-                printf("----%s\n", dit->d_name);
-                strcpy(myStudents[*i].cfileDirPath, directoryPath);
-                strcpy(myStudents[*i].cFilePath, dit->d_name);
-                printf("%s", dit->d_name);
-                (*i)++;
-                break;
-            }*/
-        //}
+        }
     }
 
     if(closedir(dip)==FAIL){
         handleFailure();
     }
-    //return myStudents;
 }
 
+/**
+ * findTheCFile function.
+ * @param subDir - sub dir path
+ * @param myStudents - students arr
+ * @param i - num of students
+ * @param cPath - c file path
+ */
 void findTheCFile(char subDir[INPUT_SIZE],students* myStudents,int i,char cPath[INPUT_SIZE]){
     DIR* dip;
     struct dirent* dit;
     if((dip=opendir(subDir))==NULL){
         handleFailure();
     }
-
+    //read from the dir
     while ((dit=readdir(dip))!=NULL) {
 
         if (dit->d_type == DT_REG) {
             int len = strlen(dit->d_name);
-            printf("%s\n", dit->d_name);
             if (len >= 2 && dit->d_name[len - 1] == 'c' && dit->d_name[len - 2] == '.') {
-                printf("----%s\n", dit->d_name);
                 strcpy(myStudents[i].cfileDirPath, subDir);
-
                 strcpy(cPath,dit->d_name);
                 break;
             }
@@ -379,7 +360,6 @@ void findTheCFile(char subDir[INPUT_SIZE],students* myStudents,int i,char cPath[
             //concat the subDir path
             char subDirectory[INPUT_SIZE]={};
             strcpy(subDirectory,subDir);
-
             int len = strlen(subDirectory);
             if (subDirectory[len]!='/') {
                 strcat(subDirectory,"/");
@@ -400,14 +380,19 @@ void handleFailure() {
     exit(FAIL);
 }
 
-
-int readCMDFile(char* cmdFile,char directoryPath[INPUT_SIZE], char inputFilePath[INPUT_SIZE],char outputFilePath [INPUT_SIZE]) {
+/**
+ * readCMDFile function.
+ * @param cmdFile - the configuration file
+ * @param directoryPath - dir path
+ * @param inputFilePath - input path
+ * @param outputFilePath - output path
+ */
+void readCMDFile(char* cmdFile,char directoryPath[INPUT_SIZE], char inputFilePath[INPUT_SIZE],char outputFilePath [INPUT_SIZE]) {
     //open files
     int file = open(cmdFile, O_RDONLY);
     if (file < 0) {
         handleFailure();
     }
-
     char buffer[BUFF_SIZE];
     int status;
     int count;
@@ -429,7 +414,6 @@ int readCMDFile(char* cmdFile,char directoryPath[INPUT_SIZE], char inputFilePath
                     case UPDATE3:
                         outputFilePath[j] = buffer[i];
                         break;
-
                 }
                 j++;
 
